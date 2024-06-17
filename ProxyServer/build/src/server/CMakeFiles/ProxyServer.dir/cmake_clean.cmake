3d28e3e660c7bbc2f07d71d720c0b2b7c9246e07
mzy0727 <1795498057@qq.com>
2024-06-17 14:40:50 +0800

    全新架构，拆分服务

---
+++ ProxyServer/build/src/server/CMakeFiles/ProxyServer.dir/cmake_clean.cmake
@@ -0,0 +1,20 @@
+file(REMOVE_RECURSE
+  "../../../bin/ProxyServer"
+  "../../../bin/ProxyServer.pdb"
+  "CMakeFiles/ProxyServer.dir/ChatServer.cc.o"
+  "CMakeFiles/ProxyServer.dir/ChatService.cc.o"
+  "CMakeFiles/ProxyServer.dir/db/ConnectionPool.cc.o"
+  "CMakeFiles/ProxyServer.dir/db/db.cc.o"
+  "CMakeFiles/ProxyServer.dir/model/FriendModel.cc.o"
+  "CMakeFiles/ProxyServer.dir/model/GroupModel.cc.o"
+  "CMakeFiles/ProxyServer.dir/model/OfflineMsgModel.cc.o"
+  "CMakeFiles/ProxyServer.dir/model/UserModel.cc.o"
+  "CMakeFiles/ProxyServer.dir/protobuf/DistributedService.pb.cc.o"
+  "CMakeFiles/ProxyServer.dir/redis/redis.cc.o"
+  "CMakeFiles/ProxyServer.dir/server.cc.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ProxyServer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
